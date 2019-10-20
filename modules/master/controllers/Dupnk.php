d2ccd3f9ab65fe58c23880d6a73b3e4f9682ad1e
Lahir Wisada Santoso <lahirwisada@gmail.com>
2019-10-20 14:48:01 +0700

    set post name

--- modules/master/controllers/Dupnk.php
+++ modules/master/controllers/Dupnk.php
@@ -22,28 +22,38 @@ class Dupnk extends Skparsiparis_main {
         $this->get_attention_message_from_session();
         $this->model_dupnk->change_offset_param("currpage_kelola_dupnk");
         $records = $this->model_dupnk->all();
-        
+
         $paging_set = $this->get_paging($this->get_current_location(), $records->record_found, $this->default_limit_paging, "kelola_dupnk");
         $this->set('records', $records->record_set);
         $this->set("keyword", $records->keyword);
         $this->set('field_id', $this->model_backbone_modul->primary_key);
         $this->set("paging_set", $paging_set);
         $this->set("header_title", "Master DUPNK");
-        
+
         $this->set("additional_js", "back_bone/modul/" . $this->_layout . "/js/index_js");
-        
+
         $this->set("bread_crumb", array(
             "#" => 'Daftar Modul'
         ));
-        
+
         $this->set("next_list_number", $this->model_dupnk->get_next_record_number_list());
     }
-    
+
     public function detail($id = FALSE, $posted_data = array()) {
-        parent::detail($id, $posted_data);
+        parent::detail($id, array(
+            "kode_nomor",
+            "no_dupnk",
+            "deskripsi_dupnk",
+            "turunan_dari",
+            "no_urut",
+            "jabfungsional"));
         $this->set('enum_jabatan', $this->model_dupnk->enum_jabatan);
     }
-    
+
+    protected function redirect_after_save_detail($id) {
+        redirect('master/' . $this->_name);
+    }
+
     public function get_like() {
         $keyword = $this->input->post("keyword");
         $kelompok_found = $this->model_dupnk->get_like($keyword);
