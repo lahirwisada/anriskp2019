2793ec41c59b4de08353f761fcf238b9fd31dc30
Lahir Wisada Santoso <lahirwisada@gmail.com>
2019-10-21 11:58:13 +0700

    add jabatan_fungsional to compare

--- modules/master/models/Model_Dupnk.php
+++ modules/master/models/Model_Dupnk.php
@@ -12,18 +12,18 @@ if (!defined("BASEPATH")) {
 class Model_Dupnk extends Master_Dupnk {
 
     public $enum_jabatan = array(
-        'mahir' => 'Mahir', 
-        'muda' => 'Muda', 
-        'penyelia' => "Penyelia", 
-        'pertama' => 'Pertama', 
+        'mahir' => 'Mahir',
+        'muda' => 'Muda',
+        'penyelia' => "Penyelia",
+        'pertama' => 'Pertama',
         'terampil' => 'Terampil'
     );
 
     public function __construct() {
         parent::__construct();
     }
-    
-    public function get_enum_jabatan_as_array(){
+
+    public function get_enum_jabatan_as_array() {
         return array_keys($this->enum_jabatan);
     }
 
@@ -35,11 +35,13 @@ class Model_Dupnk extends Master_Dupnk {
                         ), FALSE, TRUE, FALSE, 1, TRUE, $force_limit, $force_offset);
     }
 
-    public function get_like($keyword = FALSE) {
+    public function get_like($keyword = FALSE, $jabatan_fungsional = FALSE) {
         $result = FALSE;
         if ($keyword) {
-//            $this->db->where(" lower(" . $this->table_name . ".aktifitas_kode) LIKE lower('%" . $keyword . "%') OR lower(" . $this->table_name . ".aktifitas_nama) LIKE lower('%" . $keyword . "%')", NULL, FALSE);
-            $this->db->where(" lower(" . $this->table_name . ".kode_nomor) LIKE lower('%" . $keyword . "%') OR  lower(" . $this->table_name . ".deskripsi_dupnk) LIKE lower('%" . $keyword . "%')", NULL, FALSE);
+            if ($jabatan_fungsional && !is_null($jabatan_fungsional)) {
+                $this->db->where(" lower(" . $this->table_name . ".jabfungsional) = lower('" . $jabatan_fungsional . "')", NULL, FALSE);
+            }
+            $this->db->where("( lower(" . $this->table_name . ".kode_nomor) LIKE lower('%" . $keyword . "%') OR  lower(" . $this->table_name . ".deskripsi_dupnk) LIKE lower('%" . $keyword . "%'))", NULL, FALSE);
             $this->db->order_by("deskripsi_dupnk", "asc");
             $result = $this->get_where();
         }
