0d05bbca6cc9c540a61cd6afdb1d51e18ac47b59
Lahir Wisada Santoso <lahirwisada@gmail.com>
2019-10-21 17:37:09 +0700

    remove unused after_save function

--- controllers/Skp.php
+++ controllers/Skp.php
@@ -1,8 +1,10 @@
 <?php
-if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class Skp extends Skarsiparis_cmain {
-    
+
     public $model = 'model_tr_skp_tahunan';
     protected $auto_load_model = TRUE;
 
@@ -16,12 +18,12 @@ class Skp extends Skarsiparis_cmain {
         $thn = $this->input->get('tahun', TRUE);
         $tahun = $thn ? $thn : date('Y');
         $this->get_attention_message_from_session();
-//        $records = $this->model_tr_skp_tahunan->all_skp_plan($this->pegawai_id, $tahun);
-        $records = (object) array(
-                    "record_set" => FALSE,
-                    "record_found" => 0,
-                    "keyword" => ''
-        );;
+        $records = $this->model_tr_skp_tahunan->all_skp_plan($this->id_pegawai, $tahun);
+//        $records = (object) array(
+//                    "record_set" => FALSE,
+//                    "record_found" => 0,
+//                    "keyword" => ''
+//        );
         $this->set('records', $records->record_set);
         $this->set('total_record', $records->record_found);
         $this->set('keyword', $records->keyword);
@@ -33,18 +35,28 @@ class Skp extends Skarsiparis_cmain {
     }
 
     public function detail($id = false, $posted_data = [], $parent_id = false) {
-        parent::detail($id, array("pegawai_id", "skpt_tahun", "skpt_kegiatan", "skpt_waktu", "skpt_kuantitas", "skpt_output", "skpt_kualitas", "skpt_biaya"));
+        parent::detail($id, array(
+            "id_pegawai",
+            "skpt_tahun",
+            "id_dupnk",
+            "skpt_waktu",
+            "skpt_kuantitas",
+            "skpt_output",
+            "skpt_kualitas",
+            "skpt_biaya"
+        ));
+        
 //        $this->set('pegawai_id', $this->pegawai_id);
-        $this->set('pegawai_id', 1);
+        $this->set('pegawai_id', $this->user_detail["id_pegawai"]);
 //        $this->set('skpb', $this->model_tr_skp_bulanan->get_data_setahun($id));
         $this->set('skpb', FALSE);
         $this->set("bread_crumb", array(
             "back_end/" . $this->_name => $this->_header_title,
             "#" => 'Formulir ' . $this->_header_title
         ));
-        
+
         $this->set("additional_js", "skp/js/detail_js");
-        
+
         $this->add_cssfiles(array("plugins/select2/select2.min.css"));
         $this->add_jsfiles(array("plugins/select2/select2.full.min.js"));
 //        $this->add_jsfiles(array("plugins/smartwizard/jquery.smartWizard-2.0.min.js"));
@@ -81,23 +93,23 @@ class Skp extends Skarsiparis_cmain {
     }
 
     protected function after_save($id = FALSE, $saved_id = FALSE) {
-        $idk = $id ? $id : $saved_id;
-        $kualitas = $this->input->post('skpt_kualitas', TRUE);
-        $kuantitas = $this->input->post('kuantitas', TRUE);
-        $biaya = $this->input->post('biaya');
-        $data = array();
-        for ($i = 1; $i <= 12; $i++) {
-            $data[] = array(
-                'skpt_id' => $idk,
-                'skpb_bulan' => $i,
-                'skpb_kuantitas' => $kuantitas[$i],
-                'skpb_kualitas' => $kualitas,
-                'skpb_biaya' => $biaya[$i],
-                'created_by' => $this->user_detail['username']
-            );
-        }
-        $this->model_tr_skp_bulanan->save_data($id, $data);
+//        $idk = $id ? $id : $saved_id;
+//        $kualitas = $this->input->post('skpt_kualitas', TRUE);
+//        $kuantitas = $this->input->post('kuantitas', TRUE);
+//        $biaya = $this->input->post('biaya');
+//        $data = array();
+//        for ($i = 1; $i <= 12; $i++) {
+//            $data[] = array(
+//                'skpt_id' => $idk,
+//                'skpb_bulan' => $i,
+//                'skpb_kuantitas' => $kuantitas[$i],
+//                'skpb_kualitas' => $kualitas,
+//                'skpb_biaya' => $biaya[$i],
+//                'created_by' => $this->user_detail['username']
+//            );
+//        }
+//        $this->model_tr_skp_bulanan->save_data($id, $data);
         unset($data);
     }
-    
+
 }
